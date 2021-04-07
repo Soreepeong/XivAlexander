38c39b91b59e6e59d824a2931194f83f49754177
John Olano <jjolano@me.com>
2021-04-06 21:53:15 -0400

    add more description for penalty calculation

--- XivAlexander/App_Feature_AnimationLockLatencyHandler.cpp
+++ XivAlexander/App_Feature_AnimationLockLatencyHandler.cpp
@@ -182,6 +182,7 @@ public:
 												extraMessage += Utils::FormatString("/%lldms/%lldms", latency_med, latency_dev);
 
 												// Calculate penalty from standard deviation of ping or using BaseLatencyPenalty
+												// If user's ping is lower than the setting, simulate their expected ping from observed statistics.
 												const int64_t latency_base = std::min(int64_t(config.BaseLatencyPenalty) - latency_dev, latency_med + latency_dev);
 												const int64_t penalty = std::max(latency_base / 2, latency_dev);
 
