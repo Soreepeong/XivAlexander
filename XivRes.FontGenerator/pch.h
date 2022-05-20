9ca1104c5e8b17da6aa6131040f9c2945fe96dfc
Soreepeong <soreepeong@gmail.com>
2022-05-21 03:03:06 +0900

    pch

---
+++ XivRes.FontGenerator/pch.h
@@ -0,0 +1,22 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <iostream>
+#include <Windows.h>
+#include <windowsx.h>
+#include <CommCtrl.h>
+
+#pragma comment(lib, "Comctl32.lib")
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include FT_BITMAP_H
+#include FT_OUTLINE_H 
+#include FT_GLYPH_H
+
+#include "XivRes/FontdataStream.h"
+#include "XivRes/GameReader.h"
+#include "XivRes/MipmapStream.h"
+#include "XivRes/PackedFileUnpackingStream.h"
+#include "XivRes/PixelFormats.h"
+#include "XivRes/TextureStream.h"
+#include "XivRes/Internal/TexturePreview.Windows.h"
