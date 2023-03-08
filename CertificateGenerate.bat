c3577119a246f10339e05dcdb648af3f5a0c3253
Soreepeong <soreepeong@gmail.com>
2023-03-08 14:47:28 +0900

    Use relative path for CertificateGenerate

--- CertificateGenerate.bat
+++ CertificateGenerate.bat
@@ -3,8 +3,8 @@ if exist Build\Certificate.pfx goto :EOF
 set /p CN="Enter CN: "
 set /p CERT_PASSWORD="Enter PFX Password: "
 echo %CERT_PASSWORD%> Build\CertificatePassword.txt
-set MAKECERT="C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\makecert.exe"
-set PVK2PFX="C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\pvk2pfx.exe"
+set MAKECERT="makecert.exe"
+set PVK2PFX="pvk2pfx.exe"
 %MAKECERT% -eku 1.3.6.1.5.5.7.3.3 -r -pe -n "CN=%CN%" -a sha512 -len 4096 -cy authority -sv Build\CARoot.pvk Build\CARoot.cer
 %MAKECERT% -eku 1.3.6.1.5.5.7.3.3 -pe -n "CN=%CN%" -iv Build\CARoot.pvk -ic Build\CARoot.cer -a sha512 -len 4096 -cy end -sky signature -sv Build\Certificate.pvk Build\Certificate.cer
 %PVK2PFX% -pvk Build\CARoot.pvk -spc Build\CARoot.cer -pfx Build\CARoot.pfx -po "%CERT_PASSWORD%"
