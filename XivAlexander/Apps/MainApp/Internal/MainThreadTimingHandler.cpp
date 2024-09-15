6cb80f61acd155cd112ff8017c24472c72d99d4a
Soreepeong <soreepeong@gmail.com>
2024-09-15 22:25:41 +0900

    Use targeted hook instead of PeekMessage hook if available

--- XivAlexander/Apps/MainApp/Internal/MainThreadTimingHandler.cpp
+++ XivAlexander/Apps/MainApp/Internal/MainThreadTimingHandler.cpp
@@ -1,6 +1,7 @@
 ﻿#include "pch.h"
 
 #include <XivAlexanderCommon/Utils/CallOnDestruction.h>
+#include <XivAlexanderCommon/Utils/Signatures.h>
 
 #include "Config.h"
 #include "Apps/MainApp/App.h"
@@ -8,13 +9,16 @@
 #include "Apps/MainApp/Internal/NetworkTimingHandler.h"
 #include "Apps/MainApp/Internal/SocketHook.h"
 #include "Misc/Hooks.h"
+#include "Misc/Logger.h"
 
 static constexpr auto SecondToMicrosecondMultiplier = 1000000ULL;
 
 struct XivAlexander::Apps::MainApp::Internal::MainThreadTimingHandler::Implementation {
 	Apps::MainApp::App& App;
 	const std::shared_ptr<Config> Config;
+	const std::shared_ptr<Misc::Logger> Logger;
 
+	std::optional<Misc::Hooks::PointerFunction<bool>> SingleMessageLoop;
 	Misc::Hooks::ImportedFunction<BOOL, LPMSG, HWND, UINT, UINT, UINT> PeekMessageW{ "user32!PeekMessageW", "user32.dll", "PeekMessageW" };
 	Misc::Hooks::ImportedFunction<void, DWORD> Sleep{ "kernel32!Sleep", "kernel32.dll", "Sleep" };
 	Misc::Hooks::ImportedFunction<DWORD, DWORD, BOOL> SleepEx{ "kernel32!SleepEx", "kernel32.dll", "SleepEx" };
@@ -31,93 +35,33 @@ struct XivAlexander::Apps::MainApp::Internal::MainThreadTimingHandler::Implement
 
 	Implementation(Apps::MainApp::App& app)
 		: App(app)
-		, Config(Config::Acquire()) {
+		, Config(Config::Acquire())
+		, Logger(Misc::Logger::Acquire()) {
 
-		Cleanup += PeekMessageW.SetHook([this](LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax, UINT wRemoveMsg) mutable->BOOL {
-			if (App.IsRunningOnGameMainThread()) {
-				if (LastPeekMessageHadRemoveMsg == wRemoveMsg) {
-					const auto& rt = Config->Runtime;
+		try {
+			const auto base = reinterpret_cast<char*>(GetModuleHandleW(nullptr));
+			const auto sizeOfImage = reinterpret_cast<IMAGE_NT_HEADERS*>(base + reinterpret_cast<IMAGE_DOS_HEADER*>(base)->e_lfanew)->OptionalHeader.SizeOfImage;
+			const auto mainModuleSpan = std::span<char>(base, sizeOfImage);
+			const auto messageLoopSignature = Utils::Signatures::RegexSignature(R"(\xE8(....)\x84\xc0\x75\xf7)");
+			Utils::Signatures::ScanResult sr;
+			if (!messageLoopSignature.Lookup(mainModuleSpan, sr))
+				throw std::runtime_error(__FUNCTION__ ": failed to find message loop");
+			SingleMessageLoop.emplace("SingleMessageLoop", sr.ResolveAddress<bool(*)()>(1));
+			Cleanup += SingleMessageLoop->SetHook([this]() { BeforePeekMessage(); return SingleMessageLoop->bridge(); });
 
-					auto nowUs = Utils::QpcUs();
+		} catch (const std::exception& e) {
+			Logger->Format<LogLevel::Info>(LogCategory::General, L"Hooking PeekMessageW instead of message loop: {}", e.what());
 
-					while (!MessagePumpGuaranteeCounterUs.empty() && *MessagePumpGuaranteeCounterUs.begin() <= nowUs)
-						MessagePumpGuaranteeCounterUs.erase(MessagePumpGuaranteeCounterUs.begin());
-
-					int64_t waitUntilCounterUs = 0;
-					if (!MessagePumpGuaranteeCounterUs.empty() && *MessagePumpGuaranteeCounterUs.begin() - nowUs <= MessagePumpIntervalTrackerUs.Latest()) {
-						waitUntilCounterUs = *MessagePumpGuaranteeCounterUs.begin();
-						MessagePumpGuaranteeCounterUs.erase(MessagePumpGuaranteeCounterUs.begin());
-					}
-
-					auto recordPumpInterval = false;
-					if (!waitUntilCounterUs) {
-						uint64_t waitForUs = 0, waitForDrift = 0;
-						const auto test = Utils::QpcUs();
-						if (const auto& networkingHelper = App.GetNetworkTimingHandler(); networkingHelper && rt.LockFramerateAutomatic) {
-							const auto& group = networkingHelper->GetCooldownGroup(Apps::MainApp::Internal::NetworkTimingHandler::CooldownGroup::Id_Gcd);
-							if (group.DurationUs != UINT64_MAX) {
-								const auto frameInterval = static_cast<int64_t>(Config::RuntimeRepository::CalculateLockFramerateIntervalUs(
-									rt.LockFramerateTargetFramerateRangeFrom,
-									rt.LockFramerateTargetFramerateRangeTo,
-									group.DurationUs,
-									rt.LockFramerateMaximumRenderIntervalDeviation
-								));
-								if (frameInterval && LastLockedFramerateRenderIntervalUs && LastLockedFramerateRenderIntervalUs != frameInterval) {
-									const auto prevRenderTimestamp = nowUs / LastLockedFramerateRenderIntervalUs * LastLockedFramerateRenderIntervalUs + LastLockedFramerateRenderDriftUs + frameInterval;
-									int64_t minDiff = UINT64_MAX;
-									int64_t minDriftUs = 0;
-									for (int64_t i = 0; i < frameInterval; ++i) {
-										const auto nextRenderTimestamp = static_cast<int64_t>((1 + (nowUs - i) / frameInterval) * frameInterval + i);
-										if (nextRenderTimestamp < prevRenderTimestamp)
-											continue;
-										const auto diff = nextRenderTimestamp - prevRenderTimestamp;
-										if (diff < minDiff) {
-											minDiff = diff;
-											minDriftUs = i;
-										}
-									}
-									LastLockedFramerateRenderDriftUs = minDriftUs;
-								}
-								waitForUs = LastLockedFramerateRenderIntervalUs = frameInterval;
-								waitForDrift = LastLockedFramerateRenderDriftUs;
-							} else {
-								waitForUs = LastLockedFramerateRenderIntervalUs = static_cast<uint64_t>(1000000. / std::min(1000000., std::max(1., rt.LockFramerateTargetFramerateRangeTo.Value())));
-								waitForDrift = 0;
-							}
-						} else {
-							waitForUs = rt.LockFramerateInterval;
-							waitForDrift = 0;
-						}
-						if (waitForUs) {
-							recordPumpInterval = true;
-							waitUntilCounterUs = (1 + (nowUs - waitForDrift) / waitForUs) * waitForUs + waitForDrift;
-						}
-					}
-
-					if (waitUntilCounterUs > 0 && !LastMessagePumpCounterUs.empty()) {
-						const auto useMoreCpuTime = rt.UseMoreCpuTime.Value();
-						while (waitUntilCounterUs > (nowUs = Utils::QpcUs())) {
-							if (useMoreCpuTime)
-								void(0);
-							else
-								::Sleep(0);
-						}
-						LastMessagePumpCounterUs.push_back(nowUs);
-					} else {
-						LastMessagePumpCounterUs.push_back(nowUs);
-						recordPumpInterval = true;
-					}
-					if (recordPumpInterval && LastMessagePumpCounterUs.size() >= 2)
-						MessagePumpIntervalTrackerUs.AddValue(LastMessagePumpCounterUs[LastMessagePumpCounterUs.size() - 1] - LastMessagePumpCounterUs[LastMessagePumpCounterUs.size() - 2]);
-					if (LastMessagePumpCounterUs.size() > 2 && LastMessagePumpCounterUs.back() - LastMessagePumpCounterUs.front() >= SecondToMicrosecondMultiplier)
-						LastMessagePumpCounterUs.pop_front();
-
-				} else {
-					LastPeekMessageHadRemoveMsg = wRemoveMsg;
+			Cleanup += PeekMessageW.SetHook([this](LPMSG lpMsg, HWND hWnd, UINT wMsgFilterMin, UINT wMsgFilterMax, UINT wRemoveMsg) mutable->BOOL {
+				if (App.IsRunningOnGameMainThread()) {
+					if (LastPeekMessageHadRemoveMsg == wRemoveMsg)
+						BeforePeekMessage();
+					else
+						LastPeekMessageHadRemoveMsg = wRemoveMsg;
 				}
-			}
-			return PeekMessageW.bridge(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax, wRemoveMsg);
-			});
+				return PeekMessageW.bridge(lpMsg, hWnd, wMsgFilterMin, wMsgFilterMax, wRemoveMsg);
+				});
+		}
 
 		Cleanup += Sleep.SetHook([&](DWORD dwMilliseconds) {
 			if (!ShouldSkipSleep(dwMilliseconds))
@@ -148,6 +92,84 @@ struct XivAlexander::Apps::MainApp::Internal::MainThreadTimingHandler::Implement
 		if (!++s_counter)
 			SwitchToThread();
 		return true;
+	}
+
+	void BeforePeekMessage() {
+		const auto& rt = Config->Runtime;
+
+		auto nowUs = Utils::QpcUs();
+
+		while (!MessagePumpGuaranteeCounterUs.empty() && *MessagePumpGuaranteeCounterUs.begin() <= nowUs)
+			MessagePumpGuaranteeCounterUs.erase(MessagePumpGuaranteeCounterUs.begin());
+
+		int64_t waitUntilCounterUs = 0;
+		if (!MessagePumpGuaranteeCounterUs.empty() && *MessagePumpGuaranteeCounterUs.begin() - nowUs <= MessagePumpIntervalTrackerUs.Latest()) {
+			waitUntilCounterUs = *MessagePumpGuaranteeCounterUs.begin();
+			MessagePumpGuaranteeCounterUs.erase(MessagePumpGuaranteeCounterUs.begin());
+		}
+
+		auto recordPumpInterval = false;
+		if (!waitUntilCounterUs) {
+			uint64_t waitForUs = 0, waitForDrift = 0;
+			const auto test = Utils::QpcUs();
+			if (const auto& networkingHelper = App.GetNetworkTimingHandler(); networkingHelper && rt.LockFramerateAutomatic) {
+				const auto& group = networkingHelper->GetCooldownGroup(Apps::MainApp::Internal::NetworkTimingHandler::CooldownGroup::Id_Gcd);
+				if (group.DurationUs != UINT64_MAX) {
+					const auto frameInterval = static_cast<int64_t>(Config::RuntimeRepository::CalculateLockFramerateIntervalUs(
+						rt.LockFramerateTargetFramerateRangeFrom,
+						rt.LockFramerateTargetFramerateRangeTo,
+						group.DurationUs,
+						rt.LockFramerateMaximumRenderIntervalDeviation
+					));
+					if (frameInterval && LastLockedFramerateRenderIntervalUs && LastLockedFramerateRenderIntervalUs != frameInterval) {
+						const auto prevRenderTimestamp = nowUs / LastLockedFramerateRenderIntervalUs * LastLockedFramerateRenderIntervalUs + LastLockedFramerateRenderDriftUs + frameInterval;
+						int64_t minDiff = UINT64_MAX;
+						int64_t minDriftUs = 0;
+						for (int64_t i = 0; i < frameInterval; ++i) {
+							const auto nextRenderTimestamp = static_cast<int64_t>((1 + (nowUs - i) / frameInterval) * frameInterval + i);
+							if (nextRenderTimestamp < prevRenderTimestamp)
+								continue;
+							const auto diff = nextRenderTimestamp - prevRenderTimestamp;
+							if (diff < minDiff) {
+								minDiff = diff;
+								minDriftUs = i;
+							}
+						}
+						LastLockedFramerateRenderDriftUs = minDriftUs;
+					}
+					waitForUs = LastLockedFramerateRenderIntervalUs = frameInterval;
+					waitForDrift = LastLockedFramerateRenderDriftUs;
+				} else {
+					waitForUs = LastLockedFramerateRenderIntervalUs = static_cast<uint64_t>(1000000. / std::min(1000000., std::max(1., rt.LockFramerateTargetFramerateRangeTo.Value())));
+					waitForDrift = 0;
+				}
+			} else {
+				waitForUs = rt.LockFramerateInterval;
+				waitForDrift = 0;
+			}
+			if (waitForUs) {
+				recordPumpInterval = true;
+				waitUntilCounterUs = (1 + (nowUs - waitForDrift) / waitForUs) * waitForUs + waitForDrift;
+			}
+		}
+
+		if (waitUntilCounterUs > 0 && !LastMessagePumpCounterUs.empty()) {
+			const auto useMoreCpuTime = rt.UseMoreCpuTime.Value();
+			while (waitUntilCounterUs > (nowUs = Utils::QpcUs())) {
+				if (useMoreCpuTime)
+					void(0);
+				else
+					::Sleep(0);
+			}
+			LastMessagePumpCounterUs.push_back(nowUs);
+		} else {
+			LastMessagePumpCounterUs.push_back(nowUs);
+			recordPumpInterval = true;
+		}
+		if (recordPumpInterval && LastMessagePumpCounterUs.size() >= 2)
+			MessagePumpIntervalTrackerUs.AddValue(LastMessagePumpCounterUs[LastMessagePumpCounterUs.size() - 1] - LastMessagePumpCounterUs[LastMessagePumpCounterUs.size() - 2]);
+		if (LastMessagePumpCounterUs.size() > 2 && LastMessagePumpCounterUs.back() - LastMessagePumpCounterUs.front() >= SecondToMicrosecondMultiplier)
+			LastMessagePumpCounterUs.pop_front();
 	}
 };
 
